dff4948fd1a439590d3afc7206ef79df9c4ea4b2
mrodvl <rodionov.mvg@gmail.com>
2025-06-19 17:38:26 +0300

    new comment

--- src/main/java/org/example/authservice/dto/UserAuthDTO.java
+++ src/main/java/org/example/authservice/dto/UserAuthDTO.java
@@ -2,7 +2,9 @@ package org.example.authservice.dto;
 
 import lombok.Builder;
 import lombok.Data;
-//
+/**
+ * DTO объект безопасной передачи данных юзера для авторизации
+ * */
 @Data
 @Builder
 public class UserAuthDTO {
--- src/main/java/org/example/authservice/dto/UserRegDTO.java
+++ src/main/java/org/example/authservice/dto/UserRegDTO.java
@@ -3,7 +3,7 @@ package org.example.authservice.dto;
 import lombok.Data;
 import org.example.authservice.entity.Roles;
 /**
- * DTO объект для безопасной передачи данных ( доп.инкапсуляция )
+ * DTO объект безопасной передачи данных юзера для его регистрации
  * Не содержит методов, а только конструктор и свойства для дальнейшей реализации
  */
 @Data
